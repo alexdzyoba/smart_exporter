c4fdf49fe8d748c697d2bda2700d47374bd52e40
Alex Dzyoba <alex@dzyoba.com>
2020-05-05 15:11:26 +0300

    Convert to custom collector to avoid stale metrics
    When the device was removed from the system smart_exporter still exposes
    its metrics - the removed device appears in labels.
    
    As far as I could research this is intended behavior for Prometheus
    metrics - metrics label values are supposed to be static throughout the
    lifetime of the binary. They can appear but the removing of the metric's
    label values are not usual.
    
    But in case of smart exporter this is not uncommon - when some SMART
    alert fires up the disk will be replaced soon. Yet, the old metrics will
    remain.
    
    The correct way to make this work is to use so-called const metrics that
    are generated on each scrape.
    
    To expose const metrics we have to write custom collector by
    implementing `Collector` interface.
    
    This is what this commit does - replaces GaugeVec metrics with custom
    type SMARTMetric that implements `Collector` interface. In `Collect`
    method metrics values are created with `MustNewConstMetric`.
    
    Fixes #1.

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
-	"time"
 
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
@@ -27,49 +26,45 @@ type Blockdevice struct {
 
 type SMARTMetric struct {
 	Regexp *regexp.Regexp
-	Metric *prometheus.GaugeVec
+	Desc   *prometheus.Desc
 }
 
 var metrics = []SMARTMetric{
 	{
 		Regexp: regexp.MustCompile(`Reallocated_Sector_Ct`),
-		Metric: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "smart_reallocated_sectors_total",
-				Help: "Number of reallocated sectors",
-			},
+		Desc: prometheus.NewDesc(
+			"smart_reallocated_sectors_total",
+			"Number of reallocated sectors",
 			[]string{"device"},
+			nil,
 		),
 	},
 	{
 		Regexp: regexp.MustCompile(`^Elements in grown defect list:`),
-		Metric: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "smart_grown_defect_list_total",
-				Help: "Number of elements in grown defect list",
-			},
+		Desc: prometheus.NewDesc(
+			"smart_grown_defect_list_total",
+			"Number of elements in grown defect list",
 			[]string{"device"},
+			nil,
 		),
 	},
 	{
 
 		Regexp: regexp.MustCompile(`^read:`),
-		Metric: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "smart_read_uncorrected_errors_total",
-				Help: "Number of uncorrected read errors",
-			},
+		Desc: prometheus.NewDesc(
+			"smart_read_uncorrected_errors_total",
+			"Number of uncorrected read errors",
 			[]string{"device"},
+			nil,
 		),
 	},
 	{
 		Regexp: regexp.MustCompile(`^write:`),
-		Metric: prometheus.NewGaugeVec(
-			prometheus.GaugeOpts{
-				Name: "smart_write_uncorrected_errors_total",
-				Help: "Number of uncorrected write errors",
-			},
+		Desc: prometheus.NewDesc(
+			"smart_write_uncorrected_errors_total",
+			"Number of uncorrected write errors",
 			[]string{"device"},
+			nil,
 		),
 	},
 }
@@ -86,7 +81,7 @@ func lastField(line string) (float64, error) {
 	return val, nil
 }
 
-func collectDeviceMetrics(bd Blockdevice) {
+func (sm *SMARTMetric) collectDeviceMetrics(bd Blockdevice, ch chan<- prometheus.Metric) {
 	cmd := exec.Command("smartctl", "-i", "-A", "-l", "error", bd.Name)
 	out, err := cmd.Output()
 	if err != nil {
@@ -97,21 +92,25 @@ func collectDeviceMetrics(bd Blockdevice) {
 
 	lines := strings.Split(string(out), "\n")
 	for _, line := range lines {
-		for _, m := range metrics {
-			if m.Regexp.MatchString(line) {
-				val, err := lastField(line)
-				if err != nil {
-					log.Println(errors.Wrapf(err, "failed to parse metrics for device %s", bd.Name).Error())
-					continue
-				}
-
-				m.Metric.With(prometheus.Labels{"device": bd.Name}).Set(val)
+		if sm.Regexp.MatchString(line) {
+			val, err := lastField(line)
+			if err != nil {
+				log.Println(errors.Wrapf(err, "failed to parse metrics for device %s", bd.Name).Error())
+				continue
 			}
+
+			log.Printf("%v %v %v\n", sm.Desc, bd.Name, val)
+			ch <- prometheus.MustNewConstMetric(
+				sm.Desc,
+				prometheus.GaugeValue,
+				val,
+				bd.Name,
+			)
 		}
 	}
 }
 
-func collect() {
+func (sm SMARTMetric) Collect(ch chan<- prometheus.Metric) {
 	cmd := exec.Command("lsblk", "--nodeps", "--paths", "--json")
 	out, err := cmd.Output()
 	if err != nil {
@@ -127,32 +126,36 @@ func collect() {
 
 	for _, bd := range lsblk.Blockdevices {
 		if bd.Type == "disk" {
-			go collectDeviceMetrics(bd)
+			sm.collectDeviceMetrics(bd, ch)
 		}
 	}
 }
 
+func (sm SMARTMetric) Describe(ch chan<- *prometheus.Desc) {
+	ch <- sm.Desc
+}
+
 func main() {
 	listen := flag.String("listen-addr", ":9649", "Address to listen")
-	interval := flag.Duration("update-interval", 10*time.Minute, "SMART info update interval")
+	// interval := flag.Duration("update-interval", 10*time.Minute, "SMART info update interval")
 	flag.Parse()
 
 	for _, m := range metrics {
-		prometheus.MustRegister(m.Metric)
+		prometheus.MustRegister(m)
 	}
 
 	// Collect device metrics independent of HTTP handler
-	ticker := time.NewTicker(*interval)
-	go func() {
-		collect()
-		for range ticker.C {
-			collect()
-		}
-	}()
+	// ticker := time.NewTicker(*interval)
+	// go func() {
+	// 	collect()
+	// 	for range ticker.C {
+	// 		collect()
+	// 	}
+	// }()
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`<html>
+		_, _ = w.Write([]byte(`<html>
 			<head><title>SMART exporter</title></head>
 			<body>
 			<h1>SMART Exporter</h1>
